2b4b0851c3cd38331938548b079c02bf9c2de179
jesco00s <joseescobar197@gmail.com>
2026-01-28 11:12:06 -0600

    draft of endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("main")
-	//define routes
+	const weatherPath = "/weather"
+	var routes = []string{weatherPath}
+	port := ":8080"
 
-	//start
+	http.HandleFunc(weatherPath, weatherHandler)
 
+	fmt.Printf("Server starting (%s)\n", port)
+	fmt.Println("Routes:")
+	for i := range routes {
+		fmt.Printf("\t %s \n", routes[i])
+	}
+
+	fmt.Println("Ready")
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func weatherHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("success"))
 }
