6a5fcb9f651cdce6589e736c30a9dbedda4bdace
jesco00s <joseescobar197@gmail.com>
2026-01-28 14:22:24 -0600

    refactor

--- weather/handler.go
+++ weather/handler.go
@@ -3,19 +3,16 @@ package weather
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"time"
 )
 
-const nationalWeatherPointsUrl = "https://api.weather.gov/points/%f,%f"
-
 func WeatherHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		getWeather(w, r)
-	} else {
+	if r.Method != http.MethodPost {
 		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+		return
 	}
+
+	getWeather(w, r)
 }
 
 func getWeather(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +27,7 @@ func getWeather(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Latitude: %f, Longitude: %f\n", coords.Latitude, coords.Longitude)
 
-	points, err := fetchPoints(coords)
+	points, err := FetchPoints(r.Context(), coords)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -39,36 +36,4 @@ func getWeather(w http.ResponseWriter, r *http.Request) {
 	// Return the weather data as JSON
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(points)
-}
-
-func fetchPoints(coords Coordinates) (PointResponse, error) {
-	var pointResponse PointResponse
-	var pointsUrl = fmt.Sprintf(nationalWeatherPointsUrl, coords.Latitude, coords.Longitude)
-
-	req, err := http.NewRequest(http.MethodGet, pointsUrl, nil)
-	if err != nil {
-		return pointResponse, err
-	}
-
-	req.Header.Set("Accept", "application/geo+json")
-	client := &http.Client{Timeout: 10 * time.Second}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return pointResponse, err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		b, _ := io.ReadAll(resp.Body)
-		return pointResponse, fmt.Errorf("Error retrieving points from NWS status=%s body=%s", resp.Status, string(b))
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&pointResponse)
-	if err != nil {
-		return pointResponse, fmt.Errorf("Error unmarshalling response body. Error: %w ", err)
-	}
-
-	return pointResponse, nil
 }
---
+++ weather/nws_client.go
@@ -0,0 +1,44 @@
+package weather
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+const nationalWeatherPointsURL = "https://api.weather.gov/points/%f,%f"
+
+func FetchPoints(ctx context.Context, coords Coordinates) (PointResponse, error) {
+	var pointResponse PointResponse
+	var url = fmt.Sprintf(nationalWeatherPointsURL, coords.Latitude, coords.Longitude)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return pointResponse, err
+	}
+
+	req.Header.Set("Accept", "application/geo+json")
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return pointResponse, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		b, _ := io.ReadAll(resp.Body)
+		return pointResponse, fmt.Errorf("nws points request failed: status=%s body=%s", resp.Status, string(b))
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&pointResponse)
+	if err != nil {
+		return pointResponse, fmt.Errorf("failed to decode nws points response: %w ", err)
+	}
+
+	return pointResponse, nil
+}
